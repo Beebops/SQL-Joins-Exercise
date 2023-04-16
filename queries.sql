a9b22fc7971a0b48b82315415ca50b6d2ccc9be9
Beth Carretta <85016641+Beebops@users.noreply.github.com>
2023-04-16 19:41:14 -0400

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,15 @@
+SELECT * FROM owners
+FULL JOIN vehicles
+ON owners.id = vehicles.owner_id;
+
+SELECT first_name, last_name, COUNT(owner_id) FROM owners
+ON owners.id = vehicles.owner_id GROUP BY (first_name, last_name)
+ORDER BY first_name;
+
+SELECT first_name, last_name, ROUND(AVG(price)) as average_price, COUNT(owner_id)
+FROM owners o 
+JOIN vehicles v on o.id=v.owner_id 
+GROUP BY (first_name, last_name)
+HAVING COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000
+ORDER BY first_name DESC;
+  
